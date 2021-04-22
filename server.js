d39ab8519ecb572d6ff1fe1e31f28d72ade191d9
Tarek Behairy <tbtarek@gmail.com>
2021-04-21 23:21:19 -0400

    added packages

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3333;
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use(require('./routes/apiRoutes'));
+app.use(require('./routes/htmlRoutes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
